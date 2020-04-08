0555d29ba3a12604e82c2645506cb71fe977ad53
martinpierre <pierre.martin@ensta-bretagne.org>
2020-04-08 17:13:32 +0200

    Update DOE_motors.ino
    First commit from desktop github

--- DOE_motors.ino
+++ DOE_motors.ino
@@ -2,7 +2,7 @@
 This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
 It won't work with v1.x motor shields! Only for the v2's with built in PWM
 control
-
+// Test desktop github 
 For use with the Adafruit Motor Shield v2 
 ---->	http://www.adafruit.com/products/1438
 */
